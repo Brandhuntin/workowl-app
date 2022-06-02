b984b73e48297a2f7c3be9120519f9867715d5c0
Aditya Bhate <adityabhate305@gmail.com>
2022-06-02 17:30:51 +0530

    dashboard

--- src/components/Dashboard/View/View.js
+++ src/components/Dashboard/View/View.js
@@ -3,13 +3,21 @@ import ViewItem from "./ViewItem";
 import "./View.css";
 
 function View() {
+	const post = {
+		name: "Nikhil Parwani",
+		details: "(Startup, Finance)",
+		time: "2 hours ago",
+		content:
+			"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu",
+	};
+
 	return (
 		<div className='view-container'>
-			<ViewItem />
-			<ViewItem />
-			<ViewItem />
-			<ViewItem />
-			<ViewItem />
+			<ViewItem post={post} />
+			<ViewItem post={post} />
+			<ViewItem post={post} />
+			<ViewItem post={post} />
+			<ViewItem post={post} />
 		</div>
 	);
 }
--- src/components/Dashboard/View/ViewItem.js
+++ src/components/Dashboard/View/ViewItem.js
@@ -3,31 +3,24 @@ import "./ViewItem.css";
 import Send from "../../../media/send.svg";
 import View from "../../../media/view.svg";
 
-function ViewItem() {
+function ViewItem({ post }) {
 	return (
 		<div className='item-container'>
 			<div className='item-header'>
 				<div className='person-details'>
 					<div className='item-person'>
-						<p className='item-name'>Nikhil Parwani</p>
-						<p className='item-details'>(Education , Startups finance,)</p>
+						<p className='item-name'>{post.name}</p>
+						<p className='item-details'>{post.details}</p>
 					</div>
 					<div className='item-profile-pic'></div>
 				</div>
 
 				<div className='item-posted-time'>
-					<p>2 hours ago</p>
+					<p>{post.time}</p>
 				</div>
 			</div>
 			<div className='item-content'>
-				<p>
-					Laborum deserunt consequat nisi non Lorem laboris qui est ea qui
-					dolore sunt. Elit do ipsum pariatur irure esse occaecat proident.
-					Irure cupidatat velit voluptate non ea do ad sint id. Cillum magna
-					tempor elit ut adipisicing et ad consequat laborum ut voluptate tempor
-					laborum. Consequat duis sint incididunt non anim. Aliqua ad magna
-					aliqua nisi dolore tempor.
-				</p>
+				<p>{post.content}</p>
 			</div>
 			<div className='item-buttons'>
 				<button className='send-message'>
